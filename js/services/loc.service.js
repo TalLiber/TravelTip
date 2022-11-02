e068b96a2106f2d4c5951acdfebf0a45a29f0701
Tal Liber <tal.liber@gmail.com>
2022-11-02 15:05:08 +0200

    setLocation

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,12 +1,23 @@
 export const locService = {
-    getLocs
+    getLocs,
+    setLocation
 }
 
 
-const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+var gLocs = [
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
 ]
+
+function setLocation(name, lat, lng, createdAt) {
+    gLocs.push({
+        id: _makeId(),
+        name,
+        lat,
+        lng,
+        createdAt
+    })
+}
 
 function getLocs() {
     return new Promise((resolve, reject) => {
@@ -17,3 +28,11 @@ function getLocs() {
 }
 
 
+function _makeId(length = 6) {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    var txt = ''
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return txt
+}
